07b6e81f7e357846c9eeadab546846cd034002b7
Daria <deimoses04@gmal.com>
2023-08-23 17:06:03 +0300

    client and server

---
+++ server/db/migrations/20230823140316-create-friend.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Friends', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER, 
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      accepted: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Friends');
+  }
+};
